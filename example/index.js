a6a5c4a1c11e65dda83ad3595c011351f0ce9f72
Ionică Bizău <bizauionica@gmail.com>
2016-02-21 21:56:58 +0200

    Initial commit

---
+++ example/index.js
@@ -0,0 +1,39 @@
+const templates = require("../");
+
+console.log(templates("class-export", "MyClass"));
+// "use strict"
+//
+// module.exports = class MyClass {
+//     /**
+//      * MyClass
+//      * Sample description
+//      *
+//      * @name MyClass
+//      * @function
+//      * @param {Object} options An object containing the following fields:
+//      *
+//      *  - `fieldOne` (Number): Any number (default: `42`).
+//      *
+//      * @return MyClass The `MyClass` instance.
+//      */
+//     constructor (options) {
+//         /* do something */
+//     }
+// };
+
+console.log(templates("function-export", "foo"));
+// "use strict"
+//
+// /**
+//  * foo
+//  * Sample description
+//  *
+//  * @name foo
+//  * @function
+//  * @param {Number} a Param descrpition.
+//  * @param {Number} b Param descrpition.
+//  * @return {Number} Return description.
+//  */
+// module.exports = function foo (a, b) {
+//     return a + b;
+// };
